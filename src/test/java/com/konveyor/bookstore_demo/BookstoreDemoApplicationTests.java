a92676f6fc445b8697c2a23a75a3efeb7c084a02
Savitha Raghunathan <saveetha13@gmail.com>
2023-03-24 15:47:36 -0400

    Initial demo bookstore commit
    Signed-off-by: Savitha Raghunathan <saveetha13@gmail.com>

---
+++ src/test/java/com/konveyor/bookstore_demo/BookstoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konveyor.bookstore_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
